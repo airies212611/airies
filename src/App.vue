391a634dec785e24584d223c768c86d0f6e7c076
airies212611 <airiesconcha212611@gmail.com>
2024-09-11 16:29:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ItemList :items="products" />
+  </div>
+</template>
+
+<script>
+import ItemList from './components/ItemList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ItemList
+  },
+  data() {
+    return {
+      products: [
+        { name: 'Laptop', price: 10, isVisible: true },
+        { name: 'Iphone 14 pro max', price: 20, isVisible: true },
+        { name: 'Nike Shoes', price: 15, isVisible: true }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #fc14c6;
+}
+</style>
